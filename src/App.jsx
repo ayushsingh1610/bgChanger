7a5f4cd65a7d8ec3a0f06cc1422c9200084aec39
Ayush Singh <ayushsingh1610@outlook.com>
2025-01-02 14:31:11 +0530

    Change screen size from h:full to h:screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,60 @@
 import { useState } from "react";
 
 function App() {
-  const [color, setColor] = useState("black");
+  const [color, setColor] = useState("#242424");
 
   return (
     <div
-      className="w-full h-full duration-200"
+      className="w-full h-screen duration-200"
       style={{ backgroundColor: color }}
     >
       <div className="fixed flex flex-wrap justify-center bottom-16 inset-x-0 px-2">
-        <div className="flex justify-center flex-wrap gap-3 shadow-md bg-black px-3 py-2 rounded-xl">
+        <div className="flex justify-center flex-wrap gap-3 shadow-md bg-white px-3 py-2 rounded-xl">
           <button
             onClick={() => setColor("red")}
-            className="outline-none text-white px-8"
+            className="outline-none text-white font-semibold px-8 rounded-xl"
             style={{ backgroundColor: "red" }}
           >
             Red
           </button>
           <button
             onClick={() => setColor("purple")}
-            className="outline-none text-white px-8"
+            className="outline-none text-white font-semibold px-8 rounded-xl"
             style={{ backgroundColor: "purple" }}
           >
             Purple
           </button>
           <button
             onClick={() => setColor("yellow")}
-            className="outline-none text-black px-8"
+            className="outline-none text-black font-semibold px-8 rounded-xl"
             style={{ backgroundColor: "yellow" }}
           >
             Yellow
           </button>
           <button
-            onClick={() => setColor("f43f5e")}
-            className="outline-none text-black px-8"
+            onClick={() => setColor("#f43f5e")}
+            className="outline-none text-black font-semibold px-8 rounded-xl"
             style={{ backgroundColor: "#f43f5e" }}
           >
             Rose
           </button>
           <button
-            onClick={() => setColor("#f5d0fe")}
-            className="outline-none text-black px-8"
-            style={{ backgroundColor: "#f5d0fe" }}
+            onClick={() => setColor("#d8b4fe")}
+            className="outline-none text-black font-semibold px-8 rounded-xl"
+            style={{ backgroundColor: "#d8b4fe" }}
           >
             Lavender
           </button>
           <button
             onClick={() => setColor("#f97316")}
-            className="outline-none text-black px-8"
+            className="outline-none text-black font-semibold px-8 rounded-xl"
             style={{ backgroundColor: "#f97316" }}
           >
             Orange
           </button>
           <button
             onClick={() => setColor("blue")}
-            className="outline-none text-white px-8"
+            className="outline-none text-white font-semibold px-8 rounded-xl"
             style={{ backgroundColor: "blue" }}
           >
             Blue
@@ -65,4 +65,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
